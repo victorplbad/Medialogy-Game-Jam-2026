211c621f8e578a54bc2bd0bbd2f9a394a9bb1e55
Anton Bressendorff <126203682+AntonBressendorff@users.noreply.github.com>
2026-04-27 14:23:25 +0200

    implemented Dialouge line

--- Assets/Scripts/introductionScripts/DialougeLine.cs
+++ Assets/Scripts/introductionScripts/DialougeLine.cs
@@ -8,13 +8,16 @@ namespace DialougeSystem
     public class DialougeLine : DialougeBaseClass
     {
         [Header("Text Options")]
-        private TMP_Text textHolder;
+        // private TMP_Text textHolder;
+        [SerializeField] private TMP_Text textHolder;
         [SerializeField] private string input;
         [SerializeField]private TMP_FontAsset tmp_Font;
 
         private void Awake()
         {
-            textHolder = GetComponent<TMP_Text>(); // Get the TMP_Text component
+            // textHolder = GetComponent<TMP_Text>(); // Get the TMP_Text component
+
+            Debug.Log(textHolder);
 
             StartCoroutine(WriteText(input, textHolder, tmp_Font)); // Start the coroutine to write text
 
